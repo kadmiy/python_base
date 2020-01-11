c36c1fbd81cd0875d33183ac5ea8f3f5b214c9bd
kadmiy <kadmiy.mgn@gmail.com>
2020-01-11 13:33:23 +0500

    initial commit

---
+++ lesson_001/01_os_info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Нужно собрать информацию об операционной системе и версии пайтона
+
+# TODO запустить этот скрипт и закомитить результат его работы (файл os_info.txt)
+
+import platform
+import sys
+
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
