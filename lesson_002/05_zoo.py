8b52988f4411b6f76441225ec367b4c3af6adb5a
kadmiy <kadmiy.mgn@gmail.com>
2020-01-16 17:26:15 +0000

    Animals and Cage.

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -8,8 +8,10 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
 
+print('Default Zoo')
 print(zoo)
 
+print('Adding a bear to the Zoo')
 zoo.insert(1, 'bear')
 print(zoo)
 
@@ -17,15 +19,22 @@ print(zoo)
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
 
+print('Adding birds to the Zoo')
 zoo.extend(birds)
 print(zoo)
 
 # уберите слона
 #  и выведите список на консоль
 
+print('Removing the elephant from the Zoo')
 zoo.remove('elephant')
 print(zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+
+print('Count of Lion''s in zoo... = ', zoo.count('lion'))
+print('The lion is sitting in cage', zoo.index('lion') + 1)
+
+print('Count of Lark''s in zoo... = ', zoo.count('lark'))
+print('The lark is sitting in cage', zoo.index('lark') + 1)
