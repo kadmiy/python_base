1022948128df6a7fedfb949f06f9c6538ed65162
kadmiy <kadmiy.mgn@gmail.com>
2020-01-22 20:16:10 +0500

    SimpleDraw - Simple lined rainbow.

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -8,11 +8,25 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+start_x, start_y = 50, 50
+end_x, end_y = 350, 450
+sd.resolution = (500, 500)
+for current_color in rainbow_colors:
+    start_point = sd.get_point(start_x, start_y)
+    end_point = sd.get_point(end_x, end_y)
+    sd.line(start_point=start_point, end_point=end_point, color=current_color,width=4)
+    start_x += 5
+    # start_y += 5
+    end_x += 5
+    # end_y += 5
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+# sd.resolution = (500, 500)
+# for current_color in rainbow_colors:
+#     start_point = sd.get_point(start_x, start_y)
+#     end_point = sd.get_point(end_x, end_y)
+
 
 sd.pause()
