82a6681d4c92cb7d1989d34a2cda17bfaa1828e7
kadmiy <kadmiy.mgn@gmail.com>
2020-01-26 22:21:51 +0500

    Create 90 circle using the function and nested loops.

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -27,26 +27,47 @@ sd.resolution = (1200, 600)
 #     tmp -= 1
 
 # Нарисовать 10 пузырьков в ряд
+# def circle(circle_center, radius, circle_color):
+#     sd.circle(center_position=circle_center, radius=radius, color=circle_color, width=5)
+#
+#
+# x, y, radius, tmp, tmp_2 = 100, 100, 50, 3, 10
+# circle_color = [0, 255, 0]
+# start_x, start_y = x, y
+# while tmp_2 > 0:
+#     circle_center = sd.get_point(start_x, start_y)
+#     start_radius = radius
+#     while tmp > 0:
+#         circle(circle_center, start_radius, circle_color)
+#         start_radius = start_radius - 10
+#         tmp -= 1
+#     start_x += 75
+#     tmp = 3
+#     tmp_2 -= 1
+
+# Нарисовать три ряда по 10 пузырьков
 def circle(circle_center, radius, circle_color):
     sd.circle(center_position=circle_center, radius=radius, color=circle_color, width=5)
 
 
-x, y, radius, tmp, tmp_2 = 100, 100, 50, 3, 10
+x, y, radius, tmp, tmp_2, tmp_3 = 100, 100, 50, 3, 10, 3
 circle_color = [0, 255, 0]
 start_x, start_y = x, y
-while tmp_2 > 0:
-    circle_center = sd.get_point(start_x, start_y)
-    start_radius = radius
-    while tmp > 0:
-        circle(circle_center, start_radius, circle_color)
-        start_radius = start_radius - 10
-        tmp -= 1
-    start_x += 75
-    tmp = 3
-    tmp_2 -= 1
-
-# Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+while tmp_3 >0:
+    while tmp_2 > 0:
+        circle_center = sd.get_point(start_x, start_y)
+        start_radius = radius
+        while tmp > 0:
+            circle(circle_center, start_radius, circle_color)
+            start_radius = start_radius - 10
+            tmp -= 1
+        start_x += 75
+        tmp = 3
+        tmp_2 -= 1
+    start_x = x
+    start_y += 75
+    tmp_2 = 10
+    tmp_3 -= 1
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
