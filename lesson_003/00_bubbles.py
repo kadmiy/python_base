cbcca5921e8bdecce60e75491917e6ffa388843a
kadmiy <kadmiy.mgn@gmail.com>
2020-01-26 21:58:14 +0500

    Create 3 circle.

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -5,7 +5,13 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+x, y, radius, tmp = 100, 100, 50, 3
+circle_color = [0, 255, 0]
+while tmp > 0:
+    circle_center = sd.get_point(x, y)
+    sd.circle(center_position=circle_center, radius=radius, color=circle_color, width=5)
+    radius = radius - 10
+    tmp -= 1
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
