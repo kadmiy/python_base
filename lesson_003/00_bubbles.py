f8a3b3d158fc292c1a506ca68155f9650becff0e
kadmiy <kadmiy.mgn@gmail.com>
2020-01-26 22:19:00 +0500

    Create 30 circle using the function and nested loop.

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -15,19 +15,35 @@ sd.resolution = (1200, 600)
 #     tmp -= 1
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
+# def circle(circle_center, radius, circle_color):
+#     sd.circle(center_position=circle_center, radius=radius, color=circle_color, width=5)
+#
+# x, y, radius, tmp = 100, 100, 50, 3
+# circle_color = [0, 255, 0]
+# while tmp > 0:
+#     circle_center = sd.get_point(x, y)
+#     circle(circle_center, radius, circle_color)
+#     radius = radius - 10
+#     tmp -= 1
+
+# Нарисовать 10 пузырьков в ряд
 def circle(circle_center, radius, circle_color):
     sd.circle(center_position=circle_center, radius=radius, color=circle_color, width=5)
 
-x, y, radius, tmp = 100, 100, 50, 3
-circle_color = [0, 255, 0]
-while tmp > 0:
-    circle_center = sd.get_point(x, y)
-    circle(circle_center, radius, circle_color)
-    radius = radius - 10
-    tmp -= 1
 
-# Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+x, y, radius, tmp, tmp_2 = 100, 100, 50, 3, 10
+circle_color = [0, 255, 0]
+start_x, start_y = x, y
+while tmp_2 > 0:
+    circle_center = sd.get_point(start_x, start_y)
+    start_radius = radius
+    while tmp > 0:
+        circle(circle_center, start_radius, circle_color)
+        start_radius = start_radius - 10
+        tmp -= 1
+    start_x += 75
+    tmp = 3
+    tmp_2 -= 1
 
 # Нарисовать три ряда по 10 пузырьков
 # TODO здесь ваш код
