d33cdd1f19d3c5c787ce3b8fffbf994d2123e099
kadmiy <kadmiy.mgn@gmail.com>
2020-02-05 22:01:44 +0500

    Lesson 1 MIPT - Complete 8 tasks.

--- lesson_001_mipt/turtle_08_function_for_polygons.py
+++ lesson_001_mipt/turtle_08_function_for_polygons.py
@@ -1,22 +1,25 @@
 import turtle
+from math import cos, radians
 
 
-def polygons(n, polygon_side):
-    for j in range(n):
-        angle = 360 / n
-        turtle.left(angle)
-        turtle.forward(polygon_side)
+def polygons(f_radius, f_heads, f_angle, f_side, f_start_angle):
     turtle.penup()
-    turtle.forward(10)
-    turtle.right(90)
-    turtle.forward(10)
-    turtle.left(90)
+    turtle.forward(f_radius)
     turtle.pendown()
+    turtle.left(f_start_angle)
+    for _ in range(f_heads):
+        turtle.left(f_angle)
+        turtle.forward(f_side)
+    turtle.right(f_start_angle)
 
 
-side = 10
-turtle.shape('classic')
-turtle.speed(speed=1)
-for i in range(3, 13, 1):
-    polygons(i, side)
-    side += 15
+radius = step_radius = 15
+turtle.shape('turtle')
+# turtle.shape('classic')
+# turtle.speed(speed=1)
+for heads in range(3, 13, 1):
+    angle = 360 / heads
+    start_angle = 90 - (angle / 2)
+    side = 2 * cos(radians(start_angle)) * radius
+    polygons(step_radius, heads, angle, side, start_angle)
+    radius += step_radius
