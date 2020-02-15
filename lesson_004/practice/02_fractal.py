4a15fd4120ccf066b2455a6b7f939b4ab6c71f02
kadmiy <kadmiy.mgn@gmail.com>
2020-02-15 00:13:02 +0000

    Lesson 4 - Complete 2 practice example.

--- lesson_004/practice/02_fractal.py
+++ lesson_004/practice/02_fractal.py
@@ -2,15 +2,35 @@
 
 import simple_draw as sd
 
-sd.resolution = (600, 800)
+sd.resolution = (1200, 700)
 
 # нарисовать ветку дерева из точки (300, 5) вертикально вверх длиной 100
 
-point_0 = sd.get_point(300, 5)
+point_0 = sd.get_point(600, 5)
+angle_0 = 90
+length_0 = 200
+
+
+# v1 = sd.get_vector(start_point=point_0, angle=90, length=100)
+# v1.draw()
 
 
 # сделать функцию рисования ветки из заданной точки,
 # заданной длины, с заданным наклоном
+# def branch(point, angle, length):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#     return v1.end_point
+
+
+# next_point = branch(point=point_0, angle=angle_0, length=length_0)
+# next_angle = angle_0 - 30
+# next_length = length_0 * .75
+# next_point = branch(point=next_point, angle=next_angle, length=next_length)
+# next_angle = next_angle - 30
+# next_length = next_length * .75
+# next_point = branch(point=next_point, angle=next_angle, length=next_length)
+
 # def branch(point, angle, length):
 #     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
 #     v1.draw()
@@ -26,7 +46,17 @@ point_0 = sd.get_point(300, 5)
 # next_length = next_length * .75
 # next_point = branch(point=next_point, angle=next_angle, length=next_length)
 
+
 # написать цикл рисования ветвей с постоянным уменьшением длины на 25% и отклонением на 30 градусов
+# next_angle = angle_0
+# next_length = length_0
+# next_point = point_0
+#
+# while next_length > 10:
+#     next_point = branch(point=next_point, angle=next_angle, length=next_length)
+#     next_angle = next_angle - 30
+#     next_length = next_length * .75
+
 # angle_0 = 90
 # length_0 = 200
 #
@@ -41,6 +71,7 @@ point_0 = sd.get_point(300, 5)
 
 # сделать функцию branch рекурсивной
 
+
 def branch(point, angle, length, delta):
     if length < 1:
         return
@@ -53,10 +84,26 @@ def branch(point, angle, length, delta):
 
 
 for delta in range(0, 51, 10):
-    branch(point=point_0, angle=90, length=150, delta=delta)
+    branch(point_0, angle_0, length_0, delta)
+
 for delta in range(-50, 1, 10):
-    branch(point=point_0, angle=90, length=150, delta=delta)
+    branch(point_0, angle_0, length_0, delta)
+
+# def branch(point, angle, length, delta):
+#     if length < 1:
+#         return
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#     next_point = v1.end_point
+#     next_angle = angle - delta
+#     next_length = length * .75
+#     branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
+#
+#
+# for delta in range(0, 51, 10):
+#     branch(point=point_0, angle=90, length=150, delta=delta)
+# for delta in range(-50, 1, 10):
+#     branch(point=point_0, angle=90, length=150, delta=delta)
 
 
 sd.pause()
-
