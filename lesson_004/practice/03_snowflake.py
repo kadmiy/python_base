2a4562f9b0af5b286c02dadac610a4a667eea56c
kadmiy <kadmiy.mgn@gmail.com>
2020-02-15 00:30:31 +0000

    Lesson 4 - Complete 3 practice example.

--- lesson_004/practice/03_snowflake.py
+++ lesson_004/practice/03_snowflake.py
@@ -4,35 +4,50 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 # познакомится с параметрами библиотечной функции рисования снежинки sd.snowflake()
+# point_0 = sd.get_point(600, 500)
+x = 100
+y = 500
 
 # sd.snowflake(center=point_0, length=200, factor_a=0.5)
 
 # реализовать падение одной снежинки
-y = 500
-x = 100
-
-y2 = 450
-x2 = 150
 while True:
     sd.clear_screen()
     point = sd.get_point(x, y)
     sd.snowflake(center=point, length=50)
-    y -= 10
+    y -= 50
     if y < 50:
-       break
-    x = x + 10
-
-    point2 = sd.get_point(x2, y2)
-    sd.snowflake(center=point2, length=30)
-    y2 -= 10
-    if y2 < 50:
-       break
-    x2 = x2 + 20
-
+        break
+    x *= 1.3
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
 
+# y = 500
+# x = 100
+#
+# y2 = 450
+# x2 = 150
+# while True:
+#     sd.clear_screen()
+#     point = sd.get_point(x, y)
+#     sd.snowflake(center=point, length=50)
+#     y -= 10
+#     if y < 50:
+#        break
+#     x = x + 10
+#
+#     point2 = sd.get_point(x2, y2)
+#     sd.snowflake(center=point2, length=30)
+#     y2 -= 10
+#     if y2 < 50:
+#        break
+#     x2 = x2 + 20
+#
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
+#
 # реализовать падение одной снежинки из произвольного места экрана
 
 # реализовать падение одной снежинки с ветром - смещение в сторону
