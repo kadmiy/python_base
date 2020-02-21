341084d5b7d598f4139834f0022a33e19d2e2493
kadmiy <kadmiy.mgn@gmail.com>
2020-02-21 13:26:05 +0000

    Lesson 4 - Uncomplete task 2.

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -14,6 +14,56 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
 
-sd.pause()
+color_rainbow = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+                 sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+
+
+def vector(vector_start, length, angle):
+    v = sd.get_vector(vector_start, angle, length)
+    return v.end_point
+
+
+def polygon(point, heads, length, color):
+    angle_start = 15
+    angle_polygon = 360 / heads
+    point_polygon = point
+    color_paint = color_rainbow[int(color) - 1]
+    for _ in range(heads):
+        if _ == 0:
+            angle = angle_start
+        else:
+            angle += angle_polygon
+        if _ < (heads - 1):
+            end_point = vector(point, length, angle)
+        else:
+            end_point = point_polygon
+        sd.line(start_point=point, end_point=end_point, color=color_paint, width=1)
+        point = end_point
+
+
+# (point_start_x, point_start_y, length_start, type_of_polygon)
+start_point = [(100, 100, 150, 3), (400, 100, 150, 4), (100, 350, 100, 5), (400, 350, 100, 6)]
+color_input = 1
+
+while color_input > 0:
+    color_input = input('Возможные цвета:'
+                        '   0: Выход из программы.'
+                        '   1: Красный'
+                        '   2: Оранжевый'
+                        '   3: Жёлтый'
+                        '   4: Зелёный'
+                        '   5: Голубой'
+                        '   6: Синий'
+                        '   7: фиолетовый')
+    if int(color_input) == 0:
+        break
+    elif 0 > int(color_input) > 7:
+        return
+    # sd.quit()
+    for _ in start_point:
+        point_start = sd.get_point(_[0], _[1])
+        length_start = _[2]
+        heads_start = _[3]
+        polygon(point_start, heads_start, length_start, color_input)
+    sd.pause()
