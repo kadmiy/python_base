99ac86b7242427d76e720b8a0f4a3c66cee553e7
kadmiy <kadmiy.mgn@gmail.com>
2020-03-13 12:56:12 +0000

    Lesson 4 - Start task 5 part 1, but not completed yet.

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -21,6 +21,31 @@ N = 20
 while True:
     sd.clear_screen()
     pass
+    y = 500
+    x = 100
+
+    y2 = 450
+    x2 = 150
+    while True:
+        sd.clear_screen()
+        point = sd.get_point(x, y)
+        sd.snowflake(center=point, length=50)
+        y -= 10
+        if y < 50:
+            break
+        x = x + 10
+
+        point2 = sd.get_point(x2, y2)
+        sd.snowflake(center=point2, length=30)
+        y2 -= 10
+        if y2 < 50:
+            break
+        x2 = x2 + 20
+
+        sd.sleep(0.1)
+        if sd.user_want_exit():
+            break
+
     pass
     pass
     sd.sleep(0.1)
